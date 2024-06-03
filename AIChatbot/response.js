b06100f32b4f77f82d89910c5f1039de9914bc44
Nayanathara98 <125238773+Nayanathara98@users.noreply.github.com>
2024-06-03 01:32:59 -0700

    Add files via upload

---
+++ AIChatbot/response.js
@@ -0,0 +1,9 @@
+const responseObj = {
+	hello: "Hey! How are you?",
+	hey: "Hey! What's up",
+	today: new Date().toDateString(),
+	time: new Date().toLocaleTimeString(),
+	bankaccounts: "Saving account, current account, FD account..",
+	thankyou: "You're welcome! If you have any questions ask.",
+};
+
